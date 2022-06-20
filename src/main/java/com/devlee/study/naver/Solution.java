f75f536ccbb18e2820fa1b1bb40ac77d8f3385dd
Devlee <hjcloud@me.com>
2022-06-20 18:10:44 +0900

    [Naver] 네이버 코딩 테스트

---
+++ src/main/java/com/devlee/study/naver/Solution.java
@@ -0,0 +1,18 @@
+package com.devlee.study.naver;
+
+import org.springframework.stereotype.Component;
+
+import java.awt.*;
+import java.awt.geom.Point2D;
+import java.util.Arrays;
+
+@Component
+public class Solution {
+    public int Solution( Point2D[] A) {
+        return -1;
+    }
+
+    public void test() {
+
+    }
+}
