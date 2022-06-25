1c518329f3aec5e65f84f5efdaa907d66ecaac6c
Devlee <hjcloud@me.com>
2022-06-25 23:44:26 +0900

    [hash] HashMap 기초

--- src/main/java/com/devlee/study/proxy/ProxyRun.java
+++ src/main/java/com/devlee/study/proxy/ProxyRun.java
@@ -11,7 +11,7 @@ public class ProxyRun implements CommandLineRunner {
     public void run(String... args) {
         // Subject 클래스의 메소드를 호출하는것이아닌 프록시클래스의 메소드를 호출한다.
         Subject subject = new Proxy();
-        Proxy proxy = new Proxy();
+//        Proxy proxy = new Proxy();
 //		System.out.println(proxy.request());
         System.out.println(subject.request()); // 내부적으로 Subject 의 메소드를 호출한다.
     }
